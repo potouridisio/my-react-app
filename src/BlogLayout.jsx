6b6fe107e0b49b170e04d07562db948b96c33108
Dimitris iwannidis <iwannidisdimitrioss@gmail.com>
2023-04-30 13:41:17 +0300

    added useEffect

--- src/BlogLayout.jsx
+++ src/BlogLayout.jsx
@@ -1,42 +1,73 @@
-import { BiUserCircle } from 'react-icons/bi';
-import { FaComment } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { BiUserCircle } from "react-icons/bi";
+import { FaComment } from "react-icons/fa";
 
 const BlogLayout = () => {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="w-full md:w-1/4 p-4">
         <h1 className="text-2xl font-bold mb-4">Users</h1>
         <ul className="divide-y divide-gray-200">
-          <li className="bg-gray-100 hover:bg-gray-100 py-2 px-4 cursor-pointer">
-            <div className="flex items-center">
-              <BiUserCircle className="mr-2" size={24} />
-              <div>Leanne Graham</div>
-            </div>
-          </li>
-          <li className="hover:bg-gray-50 py-2 px-4 cursor-pointer">
-            <div className="flex items-center">
-              <BiUserCircle className="mr-2" size={24} />
-              <div>Ervin Howell</div>
-            </div>
-          </li>
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className="bg-gray-100 hover:bg-gray-100 py-2 px-4 cursor-pointer"
+            >
+              <div className="flex items-center">
+                <BiUserCircle className="mr-2" size={24} />
+                <div>{user.name}</div>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
+
       <div className="w-full md:w-3/4 p-4">
         <h1 className="text-2xl font-bold mb-4">Posts</h1>
-        {/* <div className="text-lg">No posts found for this user.</div> */}
-        <div className="mb-8">
-          <h2 className="text-xl font-bold mb-2">
-            sunt aut facere repellat provident occaecati excepturi optio reprehenderit
-          </h2>
-          <div className="text-gray-500 mb-4">by Leanne Graham</div>
-          <p className="mb-4">
-            quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas
-            totam\nnostrum rerum est autem sunt rem eveniet architecto
-          </p>
-          <div className="text-sm text-gray-500 flex items-center">
-            <FaComment className="mr-2" size={16} />5 comments
+        {posts.map((post) => (
+          <div key={post.id} className="mb-8">
+            <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+            <div className="text-gray-500 mb-4">
+              by {users[post.userId - 1]?.name}
+            </div>
+            <p className="mb-4">{post.body}</p>
+            <div className="text-sm text-gray-500 flex items-center">
+              <FaComment className="mr-2" size={16} />
+              {
+                comments.filter((comment) => comment.postId === post.id).length
+              }{" "}
+              comments
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
